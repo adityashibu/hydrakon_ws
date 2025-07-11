c076d5c0e8111214e19713ba7445b68247137ad7
abdulmaajidaga <amaajidking@gmail.com>
2025-07-11 15:08:52 +0400

    meow

--- src/lidar_cluster/build/lidar_cluster/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/aditya/hydrakon_ws/src/lidar_cluster/install/lidar_cluster'
--- src/perception_module/build/perception_module/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/aditya/hydrakon_ws/src/perception_module/install/perception_module'
